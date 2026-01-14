87593442ec6cf7215b72fe60fc7176e19c5225a3
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2026-01-14 20:20:50 +0100

    Updated sanity client file

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,11 +1,25 @@
-// sanity/lib/sanity.client.ts
+// lib/sanity.client.ts
 import { createClient } from "@sanity/client"
-import { apiVersion, dataset, projectId } from "@/sanity/env"
 
-// ✅ Sanity client
 export const sanityClient = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true, // caching for faster responses
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
+  useCdn: true,
 })
+
+
+
+
+
+// // sanity/lib/sanity.client.ts
+// import { createClient } from "@sanity/client"
+// import { apiVersion, dataset, projectId } from "@/sanity/env"
+
+// // ✅ Sanity client
+// export const sanityClient = createClient({
+//   projectId,
+//   dataset,
+//   apiVersion,
+//   useCdn: true, // caching for faster responses
+// })

423699b2763ee303510813456167e4470905e614
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2026-01-16 18:13:54 +0100

    Upgraded sanity

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,17 +1,12 @@
-'use client' // Required because NextStudio is a client component
+'use client' // Must remain if rendering NextStudio in the browser
 
 import { NextStudio } from 'next-sanity/studio'
-import config from '@/sanity.config'
+import config from '../../../sanity.config'
 
-export const dynamic = 'force-static' // Ensures static rendering
-
-export { metadata, viewport } from 'next-sanity/studio'
+export const dynamic = 'force-static' // optional, ensures static rendering
 
 export default function StudioPage() {
-  return (
-    <div className="h-screen w-screen">
-      <NextStudio config={config} />
-    </div>
-  )
+  return <NextStudio config={config} />
 }
+
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,36 @@
-// next.config.js
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   images: {
-    domains: ["cdn.sanity.io"], // ✅ add Sanity's CDN
-    unoptimized: false,         // optional: disables optimization (not recommended)
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
   },
 }
 
 export default nextConfig
+
+
+
+
+
+// // next.config.js
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   typescript: { ignoreBuildErrors: true },
+//   images: {
+//     domains: ["cdn.sanity.io"], // ✅ add Sanity's CDN
+//     unoptimized: false,         // optional: disables optimization (not recommended)
+//   },
+// }
+
+// export default nextConfig
 
 
 
